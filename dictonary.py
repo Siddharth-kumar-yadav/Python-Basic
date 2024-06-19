6e6bbd1819284a32c66a99e3d7b853b016a23215
Siddharth-kumar-yadav <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-19 15:09:54 +0530

    added dictonary

---
+++ dictonary.py
@@ -0,0 +1,7 @@
+#dictonary is printed by the help of the key information of the printed word .
+marks = {"maths":98,"Science":95,"computer":97}
+print(marks["computer"])
+marks["English"] = 96;
+print(marks)
+marks["maths"]=100;
+print(marks)
