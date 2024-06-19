f6cbdfe706e2be8f7640332d2cf885a46f509b0d
Siddharth-kumar-yadav <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-19 18:23:40 +0530

    functions code of python

---
+++ functions.py
@@ -0,0 +1,14 @@
+print("In-built Function")
+print("int()")
+print("bool()")
+print("Module Function")
+print("importing maths function")
+import math
+print(dir(math))
+from math import *
+print(sqrt(16))
+
+print("User Defined Function")
+def print_sum(first,second=9):
+    print(first +  second)
+print_sum(4)
