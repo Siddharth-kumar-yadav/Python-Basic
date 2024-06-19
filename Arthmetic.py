7b0c51e7920bcdface30525e7a4af9fc02d4d24a
Siddharth-kumar-yadav <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-19 15:07:07 +0530

    Add files via upload

---
+++ Arthmetic.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first value"))
+b = int(input("Enter second value"))
+print("Addition result is: ",a+b)
+print("Subtraction result is: ",a-b)
+print("Multiplication result is ",a*b)
+print("Division result is: ",a/b)
+print("Reminder after division result is: ",a%b)
