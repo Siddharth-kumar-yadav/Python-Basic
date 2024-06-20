25b31d025c8c0874a46a48f09284b7ac732d0099
Siddharth-kumar-yadav <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-20 18:42:57 +0530

    The string reversed
    Write a Python program that takes a string as input and returns the string reversed.

---
+++ string_rev.py
@@ -0,0 +1,11 @@
+def rev_str(input_string):
+    return input_string[::-1]
+
+# Example usage:
+input_str1 = "Siddharth"
+input_str2 = "Kumar"
+input_str3 = "Yadav"
+
+print(f"The reverse of '{input_str1}' is: '{rev_str(input_str1)}'")
+print(f"The reverse of '{input_str2}' is: '{rev_str(input_str2)}'")
+print(f"The reverse of '{input_str3}' is: '{rev_str(input_str3)}'")
