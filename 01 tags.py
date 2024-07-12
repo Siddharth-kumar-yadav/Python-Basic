1e2b932cb80379a54f4313f12a32ba3b167fbe06
Siddharth Kumar <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-07-12 20:11:40 +0530

    web scrabbing updated tags

--- 01 tags.py
+++ 01 tags.py
@@ -40,16 +40,42 @@ soup = BeautifulSoup(html_doc,'html.parser')
 # cont.string="I am a String"
 # print(cont)
 
-ulTag = soup.new_string("ul")
+# ulTag = soup.new_tag("ul")
 
-liTag = soup.new_string("li")
-liTag.string="Home"
-ulTag.append(liTag)
+# liTag = soup.new_tag("li")
+# liTag.string="Home"
+# ulTag.append(liTag)
 
-liTag = soup.new_string("li")
-liTag.string="About"
-ulTag.append(liTag)
+# liTag = soup.new_tag("li")
+# liTag.string="About"
+# ulTag.append(liTag)
 
-soup.html.body.insert(0, ulTag)
-with open("modified.html","w") as f:
-    f.write(str(soup))
+# soup.html.body.insert(0, ulTag)
+# with open("modified.html","w") as f:
+#     f.write(str(soup))
+
+# cont = soup.find(class_="container")
+# print(cont.has_attr("contenteditable"))
+
+# def has_class_but_not_id(tag):
+#     return tag.has_attr("class") and not tag.has_attr("id")
+
+# results = soup.find_all(has_class_but_not_id)
+# print(results) 
+
+# def has_class_but_not_id(tag):
+#     return not tag.has_attr("class") and not tag.has_attr("id")
+
+# results = soup.find_all(has_class_but_not_id)
+# for result in results:
+#     print(results,"\n\n")
+
+def has_class_but_not_id(tag):
+    return not tag.has_attr("class") and not tag.has_attr("id")
+
+def has_content(tag):
+    return not tag.has_attr("content")
+#results = soup.find_all(has_class_but_not_id)
+results = soup.find_all(has_content)
+for result in results:
+    print(results,"\n\n")
