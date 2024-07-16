c9b6b3c29d84832e8a397df9504423636c23ea36
Siddharth Kumar <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-07-16 19:28:30 +0530

    takin input as name by python

---
+++ name.py
@@ -0,0 +1,5 @@
+#Personal Details.
+firstname = input('Enter your first name: ')
+lastname = input('Enter your last name:')
+age = input('Enter your age : ')
+print("My Name is :", firstname+" "+lastname, "And My Age is :", age)
